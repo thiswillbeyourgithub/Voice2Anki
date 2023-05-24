a25151f2e3ee38a7c8220bee1d55118c29ebcf85
thiswillbeyourgithub <github@32mail.33mail.com>
2023-05-24 03:44:32 +0200

    demo config

--- __init__.py
+++ __init__.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     args = sys.argv[1:]
 
     to_share = False
-    auth = None
+    auth = False
+    op_br = False
+    debug = False
+    auth_message = None
 
     if args:
         whi(f"Startup arguments: {args}")
@@ -19,17 +22,37 @@ if __name__ == "__main__":
         if "auth" in args:
             auth = authentification
             yel("Authentification enabled")
+            # TODO: auth_message
+        if "open" in args:
+            of_br = True
+            yel("Opening browser")
+        if "debug" in args:
+            debug = True
+            yel("Debug mode enabled")
+
     if not to_share:
         whi("Authentification disabled")
     if not auth:
         whi("Sharing disabled")
+    if not op_br:
+        whi("Not openning browser.")
 
     if to_share and not auth:
         raise Exception("Sharing mode without authentification is disabled")
 
-    demo.queue()
+    demo.queue(concurrency_count=3)
 
     demo.launch(
             share=to_share,
             auth=auth,
+            auth_message=auth_message,
+            inbrowser=op_br
+            debug=debug,
+            show_err=True,
+            #server_name="0.0.0.0",  # TODO, this make it sharable by hand?!
+            server_port=7860,
+            show_tips=True
+            # ssl_keyfile=,
+            # ssl_certfile=,
+            # ssl_keyfile_password=,
             )
