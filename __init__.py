1819a655b7a368aba378524bc43fd9ee4fdfd5c9
thiswillbeyourgithub <github@32mail.33mail.com>
2023-05-24 02:09:58 +0200

    added init file and auth file

--- __init__.py
+++ __init__.py
@@ -1,2 +1,35 @@
+from utils.logger import whi, yel
+import sys
+from main import demo
+from utils.user_db import authentification
+
 if __name__ == "__main__":
-    
+    args = sys.argv[1:]
+
+    to_share = False
+    auth = None
+
+    if args:
+        whi(f"Startup arguments: {args}")
+
+        if "share" in args:
+            to_share = True
+            yel("Sharing enabled")
+
+        if "auth" in args:
+            auth = authentification
+            yel("Authentification enabled")
+    if not to_share:
+        whi("Authentification disabled")
+    if not auth:
+        whi("Sharing disabled")
+
+    if to_share and not auth:
+        raise Exception("Sharing mode without authentification is disabled")
+
+    demo.queue()
+
+    demo.launch(
+            share=to_share,
+            auth=auth,
+            )
--- main.py
+++ main.py
@@ -114,35 +114,6 @@ else:
     prev_audio = None
 
 
-def authentification(username, password):
-    def password_hasher(x):
-        return hashlib.sha256(x.encode()).hexdigest()
-    user_db = {
-            "g": "046db8323e882db07a6171783310495af5f5ebb47b3fd87d4db7afb30e7f2802",
-            }
-    pass_salt = "91f0e4cbbf97d6ce8cce29872ac9cacea225ed976a0c1hMiF"
-
-    # checks
-    if not (username and password):
-        red("No username of password supplied")
-        return
-    assert isinstance(username, str), "Invalid type of username"
-    assert isinstance(password, str), "Invalid type of password"
-    if username not in user_db:
-        red(f"Incorrect username '{username}'")
-        return False
-
-    # password check
-    hashed_pass = password_hasher(password + pass_salt)
-    for i in range(10_000):
-        hashed_pass = password_hasher(hashed_pass)
-    if user_db[username] == hashed_pass:
-        return True
-    else:
-        red(f"Incorrect password for user '{username}'")
-        return False
-
-
 def get_image(source):
     whi("Getting image")
     try:
@@ -700,5 +671,3 @@ with open("audio_prompts.json", "w") as f:
 messages = curate_previous_prompts(messages)
 
 demo.queue()
-#demo.launch(auth=authentification)
-demo.launch()
