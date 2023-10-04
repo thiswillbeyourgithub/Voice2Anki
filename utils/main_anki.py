8c66af15fd6414e0d95494bd6d8916dff4528a64
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-04 14:54:52 +0200

    store audio mp3 using base64

--- utils/main_anki.py
+++ utils/main_anki.py
@@ -1,3 +1,4 @@
+import base64
 import asyncio
 import joblib
 import json
@@ -128,7 +129,7 @@ def transcribe(audio_mp3_1, txt_whisp_prompt, txt_whisp_lang, txt_profile):
                                 "V2FT_profile": txt_profile,
                                 "transcribed_input": txt_audio,
                                 "model_name": f"OpenAI {modelname}",
-                                #"audio_mp3": mp3_content.decode(),
+                                "audio_mp3": base64.b64encode(mp3_content).decode(),
                                 }, db_name="anki_whisper"))
 
 
