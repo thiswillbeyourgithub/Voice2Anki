392d4869130b500c616370bdb7e59c6689592d4b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-10 19:15:50 +0200

    fix: checking equality in profile

--- utils/profiles.py
+++ utils/profiles.py
@@ -108,10 +108,15 @@ class ValueStorage:
             return default
 
     def __check_equality(self, a, b):
-        if not isinstance(a, type(b)):
+        if not (isinstance(a, type(b)) and type(a) == type(b) and isinstance(b, type(a))):
             return False
         if isinstance(a, np.ndarray):
             return (a == b).all()
+        if isinstance(a, list):
+            for i in range(len(a)):
+                if not self.__check_equality(a[i], b[i]):
+                    return False
+            return True
         return a == b
 
     def __setitem__(self, key, item):
