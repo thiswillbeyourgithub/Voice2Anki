4e828a4bec7b144ec59f913cc7267ee448417bc6
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-10 19:41:32 +0200

    turn off processing of audio by gradio

--- utils/gui_anki.py
+++ utils/gui_anki.py
@@ -173,10 +173,10 @@ with gr.Blocks(
             ]
         )
     )
-    aud_cache_event.append(audio_mp3_2.stop_recording(fn=transcribe_cache_async, inputs=[audio_mp3_2, txt_whisp_prompt, txt_whisp_lang]).then(fn=asv.n2, inputs=[txt_profile, audio_mp3_2]))
-    aud_cache_event.append(audio_mp3_3.stop_recording(fn=transcribe_cache_async, inputs=[audio_mp3_3, txt_whisp_prompt, txt_whisp_lang]).then(fn=asv.n3, inputs=[txt_profile, audio_mp3_3]))
-    aud_cache_event.append(audio_mp3_4.stop_recording(fn=transcribe_cache_async, inputs=[audio_mp3_4, txt_whisp_prompt, txt_whisp_lang]).then(fn=asv.n4, inputs=[txt_profile, audio_mp3_4]))
-    aud_cache_event.append(audio_mp3_5.stop_recording(fn=transcribe_cache_async, inputs=[audio_mp3_5, txt_whisp_prompt, txt_whisp_lang]).then(fn=asv.n5, inputs=[txt_profile, audio_mp3_5]))
+    aud_cache_event.append(audio_mp3_2.stop_recording(fn=transcribe_cache_async, inputs=[audio_mp3_2, txt_whisp_prompt, txt_whisp_lang], preprocess=False, postprocess=False).then(fn=asv.n2, inputs=[txt_profile, audio_mp3_2], preprocess=False, postprocess=False))
+    aud_cache_event.append(audio_mp3_3.stop_recording(fn=transcribe_cache_async, inputs=[audio_mp3_3, txt_whisp_prompt, txt_whisp_lang], preprocess=False, postprocess=False).then(fn=asv.n3, inputs=[txt_profile, audio_mp3_3], preprocess=False, postprocess=False))
+    aud_cache_event.append(audio_mp3_4.stop_recording(fn=transcribe_cache_async, inputs=[audio_mp3_4, txt_whisp_prompt, txt_whisp_lang], preprocess=False, postprocess=False).then(fn=asv.n4, inputs=[txt_profile, audio_mp3_4], preprocess=False, postprocess=False))
+    aud_cache_event.append(audio_mp3_5.stop_recording(fn=transcribe_cache_async, inputs=[audio_mp3_5, txt_whisp_prompt, txt_whisp_lang], preprocess=False, postprocess=False).then(fn=asv.n5, inputs=[txt_profile, audio_mp3_5], preprocess=False, postprocess=False))
 
     audio_mp3_1.clear(cancels=aud_cache_event)
     audio_mp3_2.clear(cancels=aud_cache_event)
