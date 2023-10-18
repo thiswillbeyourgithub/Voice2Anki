648fb861a8f6b852e4d6f213d1c3bb9b07fe3ad2
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-18 18:51:23 +0200

    fix: don't ask chatgpt when alfred is changing image

--- utils/main_anki.py
+++ utils/main_anki.py
@@ -230,6 +230,9 @@ def alfred(txt_audio, txt_chatgpt_context, profile, max_token, temperature, mode
     if not txt_chatgpt_context:
         return "No txt_chatgpt_context found.", [0, 0]
 
+    if "alfred" in txt_audio.lower() and "image" in txt_audio.lower() and len(txt_audio) < 40:
+        return f"Image change detected: '{txt_audio}'"
+
     prev_prompts = load_prev_prompts(profile)
     new_prompt = {
             "role": "user",
