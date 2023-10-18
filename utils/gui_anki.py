2466323533e7436d40043c949f5d28c1bd195db4
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-18 19:04:23 +0200

    dont process sounds when clearing

--- utils/gui_anki.py
+++ utils/gui_anki.py
@@ -188,7 +188,10 @@ with gr.Blocks(
             fn=reset_audio,
             cancels=aud_cache_event,
             inputs=[audio_mp3_1, audio_mp3_2, audio_mp3_3, audio_mp3_4, audio_mp3_5],
-            outputs=[audio_mp3_1, audio_mp3_2, audio_mp3_3, audio_mp3_4, audio_mp3_5])
+            outputs=[audio_mp3_1, audio_mp3_2, audio_mp3_3, audio_mp3_4, audio_mp3_5],
+            preprocess=False,
+            postprocess=False,
+            )
 
     rollaudio_btn.click(
             fn=asv.roll_audio,
