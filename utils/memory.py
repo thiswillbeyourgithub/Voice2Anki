e8a0016a7cdcac559325df930d447a623afb11e4
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-28 19:30:44 +0200

    use a faster embedding model

--- utils/memory.py
+++ utils/memory.py
@@ -53,7 +53,7 @@ else:
 
 expected_mess_keys = ["role", "content", "timestamp", "priority", "tkn_len_in", "tkn_len_out", "answer", "llm_model", "tts_model", "hash"]
 
-embedding_model_name = "paraphrase-multilingual-mpnet-base-v2"
+embedding_model_name = "paraphrase-multilingual-MiniLM-L12-v2"
 embeddings_cache = Memory(f".cache/{embedding_model_name}", verbose=0)
 embed_model = SentenceTransformer(embedding_model_name)
 
