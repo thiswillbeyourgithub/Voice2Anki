ca1402a8b00461ca26e4ac2fbc8addce45405310
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-31 12:03:16 +0100

    fix: recursive call to trim sound

--- utils/audio_splitter.py
+++ utils/audio_splitter.py
@@ -315,7 +315,10 @@ class AudioSplitter:
 
         return transcript
 
-    def trim_silences(self, audio, db_threshold=None, dbfs_threshold=-50):
+    def trim_silences(self, audio, db_threshold=None, dbfs_threshold=-50, depth=0):
+        if depth > 3:
+            red("Recursion limit of self.trim_silnces reached, not trimming this split.")
+            return audio
         # pydub's default DBFs default is -50
         whi(f"Audio length before trimming silence: {len(audio)}ms")
         assert db_threshold or dbfs_threshold, "must receive threshold"
@@ -326,8 +329,8 @@ class AudioSplitter:
         trimmed = audio[detect_leading_silence(audio, threshold):-detect_leading_silence(audio.reverse(), threshold)]
         whi(f"Audio length after trimming silence: {len(trimmed)}ms")
         if len(trimmed) <= 2000 or len(audio) / len(trimmed) >= 2:
-            red("Trimming silence of audio would be too harsh so keeping the original")
-            return audio
+            red("Trimming silence of audio would be too harsh so reducing threshold")
+            return self.trim_silences(audio, dbfs_threshold=threshold - 10, depth=depth + 1)
         else:
             return trimmed
 
