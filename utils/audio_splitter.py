f5d0119b8f789179e3b2a4cb263dd9b0da7ebfb4
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-31 13:12:56 +0100

    set condition on previoutext to False

--- utils/audio_splitter.py
+++ utils/audio_splitter.py
@@ -409,7 +409,7 @@ def whisperx_splitter(audio_path, audio_hash, prompt, language):
                 "language": language,
                 "temperature": 0,
                 "initial_prompt": prompt,
-                "condition_on_previous_text": True,
+                "condition_on_previous_text": False,
                 "word_timestamps": True,
                 "no_speech_threshold": 1,
                 },
