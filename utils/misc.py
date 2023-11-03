1f763c35f41c715b382d793b31787594cbee823c
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-03 13:08:06 +0100

    docs

--- utils/misc.py
+++ utils/misc.py
@@ -1,6 +1,6 @@
-# to know if the backend chosen by the user is anki or markdown
 class backend_config_class:
-    "module used to store information from VoiceToFormattedText.py to the main .py files"
+    """module used to store information from VoiceToFormattedText.py to
+    the main .py files"""
     def __init__(self):
         pass
 
