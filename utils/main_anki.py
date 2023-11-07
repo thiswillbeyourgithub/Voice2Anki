a149688184463799778ffccb79a6186468e82b9f
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-07 18:38:33 +0100

    Revert "fix: openai import rate limit error"
    This reverts commit 6a24dc265975b861d6cd4c59370d82c694863cc6.

--- utils/main_anki.py
+++ utils/main_anki.py
@@ -13,6 +13,7 @@ import rtoml
 import time
 from datetime import datetime
 import openai
+from openai.error import RateLimitError
 from pathlib import Path
 
 from .anki_utils import add_to_anki, audio_to_anki
@@ -98,7 +99,7 @@ def whisper_cached(
                 pv["txt_whisp_lang"] = txt_whisp_lang
 
                 return transcript
-            except openai.RateLimitError as err:
+            except RateLimitError as err:
                 if cnt >= 5:
                     return red(f"Cached whisper: RateLimitError >5: '{err}'")
                 else:
@@ -332,7 +333,7 @@ def alfred(txt_audio, txt_chatgpt_context, profile, max_token, temperature, sld_
                         user=user_identifier,
                         )
                 break
-            except openai.RateLimitError as err:
+            except RateLimitError as err:
                 if cnt >= 5:
                     return red("ChatGPT: too many retries."), [0, 0]
                 red(f"Server overloaded #{cnt}, retrying in {2 * cnt}s : '{err}'")
