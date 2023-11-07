5fd146529faa19a9bae2ab9a66cc59f6d6dfb404
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-07 18:38:27 +0100

    Revert "updated openai"
    This reverts commit d80d202fa2e9a51791c418346dda1134274f3759.

--- utils/main_anki.py
+++ utils/main_anki.py
@@ -12,6 +12,7 @@ from textwrap import dedent
 import rtoml
 import time
 from datetime import datetime
+import openai
 from pathlib import Path
 
 from .anki_utils import add_to_anki, audio_to_anki
@@ -42,10 +43,8 @@ for p in doings:
     whi(f"Starting up so moved files from doing to splitted: {p}")
     shutil.move(p, splitted_dir / p.name)
 
-from openai import OpenAI, RateLimitError
-assert Path("API_KEYS/OPENAI.txt").exists(), "No api key found"
-client = OpenAI(api_key=str(Path("API_KEYS/OPENAI.txt").read_text()).strip())
-
+assert Path("API_KEY.txt").exists(), "No api key found. Create a file API_KEY.txt and paste your openai API key inside"
+openai.api_key = str(Path("API_KEY.txt").read_text()).strip()
 
 global pv
 pv = ValueStorage()
@@ -85,7 +84,7 @@ def whisper_cached(
             try:
                 cnt += 1
                 with open(audio_path, "rb") as audio_file:
-                    transcript = openai.Audio.transcriptions.create(
+                    transcript = openai.Audio.transcribe(
                         model=modelname,
                         file=audio_file,
                         prompt=txt_whisp_prompt,
@@ -99,7 +98,7 @@ def whisper_cached(
                 pv["txt_whisp_lang"] = txt_whisp_lang
 
                 return transcript
-            except RateLimitError as err:
+            except openai.RateLimitError as err:
                 if cnt >= 5:
                     return red(f"Cached whisper: RateLimitError >5: '{err}'")
                 else:
@@ -325,7 +324,7 @@ def alfred(txt_audio, txt_chatgpt_context, profile, max_token, temperature, sld_
             try:
                 whi("Asking ChatGPT")
                 cnt += 1
-                response = client.Chat.Completions.create(
+                response = openai.ChatCompletion.create(
                         model=model_to_use,
                         messages=formatted_messages,
                         stop="END",
@@ -333,7 +332,7 @@ def alfred(txt_audio, txt_chatgpt_context, profile, max_token, temperature, sld_
                         user=user_identifier,
                         )
                 break
-            except RateLimitError as err:
+            except openai.RateLimitError as err:
                 if cnt >= 5:
                     return red("ChatGPT: too many retries."), [0, 0]
                 red(f"Server overloaded #{cnt}, retrying in {2 * cnt}s : '{err}'")
