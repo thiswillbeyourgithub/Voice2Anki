d7eae58e68bc402387ad3e491040a5ec26aa0a62
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-16 20:47:56 +0100

    fix: placeholder for keywords

--- utils/gui_anki.py
+++ utils/gui_anki.py
@@ -110,7 +110,7 @@ with gr.Blocks(
                     sld_buffer = gr.Slider(minimum=0, maximum=shared.max_message_buffer, step=1, value=shared.pv["sld_buffer"], label="Buffer size", scale=1)
                     check_gpt4 = gr.Checkbox(value=shared.pv["check_gpt4"], interactive=True, label="Use GPT4?", show_label=True, scale=0)
                     check_marked = gr.Checkbox(value=False, interactive=True, label="Mark", show_label=True, scale=0)
-                    txt_keywords = gr.Textbox(value=shared.pv["txt_keywords"], line=3, max_lines=2, label="Keywords", placeholder="Comma separated regex that, if present in the transcript, increase chances of matching memories to be selected. Each regex is stripped, case insensitive and can be used multiple times to increase the effect. If not enough memories are found, the keywords are dropped one by one starting from the last one until enough tokens are gathered.")
+                    txt_keywords = gr.Textbox(value=shared.pv["txt_keywords"], line=3, max_lines=2, label="Keywords", placeholder="Comma separated regex that, if present in the transcript, increase chances of matching memories to be selected. Each regex is stripped, case insensitive and can be used multiple times to increase the effect.")
                 txt_price = gr.Textbox(value=lambda: display_price(shared.pv["sld_max_tkn"], shared.pv["check_gpt4"]), show_label=False, interactive=False, max_lines=2, lines=2)
 
                 with gr.Row():
