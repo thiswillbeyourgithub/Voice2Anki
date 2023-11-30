680ca2c38eb5dbf7febdd88ba0ccc789276e83ca
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-30 19:04:34 +0100

    fix

--- utils/main_anki.py
+++ utils/main_anki.py
@@ -159,6 +159,7 @@ def transcribe(audio_mp3_1, txt_whisp_prompt, txt_whisp_lang, sld_whisp_temp):
         return red("Error: None whisper language")
 
     modelname = "whisper-1"
+    shared.latest_stt_used = modelname
 
     audio_mp3_1 = format_audio_component(audio_mp3_1)
 
