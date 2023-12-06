f767dcb666ac8315a7c683f336788d40cdc23c38
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-06 17:25:11 +0100

    fix: don't crash roll if alfred fails because of image

--- utils/gui_anki.py
+++ utils/gui_anki.py
@@ -271,7 +271,7 @@ with gr.Blocks(
                         inputs=[txt_audio, txt_chatgpt_context, txt_profile, sld_max_tkn, sld_temp, sld_buffer, check_gpt4, txt_keywords],
                         outputs=[txt_chatgpt_cloz],
                         queue=True,
-                        ).success(
+                        ).then(
                                 fn=dirload_splitted_last,
                                 inputs=[roll_dirload_check, txt_whisp_prompt, txt_whisp_lang, sld_whisp_temp],
                                 outputs=[audio_slots[-1]],
@@ -315,7 +315,7 @@ with gr.Blocks(
                             preprocess=False,
                             postprocess=False,
                             queue=True,
-                            ).success(
+                            ).then(
                                 fn=dirload_splitted_last,
                                 inputs=[
                                     roll_dirload_check, txt_whisp_prompt, txt_whisp_lang, sld_whisp_temp
