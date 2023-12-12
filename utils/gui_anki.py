1e0f58cb67803b44e7efc759a794cc15ec9d6fa5
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-12 12:54:47 +0100

    use button instead of html for the title

--- utils/gui_anki.py
+++ utils/gui_anki.py
@@ -87,7 +87,7 @@ with gr.Blocks(
 
     with gr.Group():
         with gr.Row():
-            gr.Button(value="VoiceToFormattedText - Anki", variant="primary", scale=3, interactive=False)
+            gr.Button(value="VoiceToFormattedText - Anki", variant="primary", scale=3, interactive=True)
             dark_mode_btn = gr.Button("Dark Mode", variant="secondary", scale=0)
             sync_btn = gr.Button(value="Sync anki", variant="secondary", scale=0)
             update_status_btn = gr.Button(value="Status", variant="secondary", scale=0)
