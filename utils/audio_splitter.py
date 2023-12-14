b296fff569e5516e1991bd5ff0ce0b1aeadd3cfd
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-14 19:03:28 +0100

    compress if file size above 19 mb

--- utils/audio_splitter.py
+++ utils/audio_splitter.py
@@ -126,7 +126,7 @@ class AudioSplitter:
         if self.compress_if_too_large:
             for i, file in enumerate(tqdm(self.to_split, unit="file", desc="Compressing")):
                 fsize = file.stat().st_size / 1024 / 1024
-                while fsize >= 20:
+                while fsize >= 19:
                     red(f"{file}'s size is {round(fsize, 3)}Mb which is > 20Mb. Compressing it now.")
                     audio = AudioSegment.from_mp3(file)
                     compressed_audio = compress_dynamic_range(audio, threshold=-20, ratio=4.0, attack=5, release=50)
