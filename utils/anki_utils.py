199a9b765a273df86bab4f4ae5c36eb320f63f6f
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-15 16:54:48 +0100

    disable trace and timeout of card status as it's faster

--- utils/anki_utils.py
+++ utils/anki_utils.py
@@ -152,7 +152,7 @@ def audio_to_anki(audio_mp3, queue):
         queue.put(red(f"\n\nError when copying audio to anki media: '{err}'"))
 
 
-@Timeout(10)
+#@Timeout(10)
 def get_card_status(txt_chatgpt_cloz):
     """return True or False depending on if the card written in
     txt_chatgpt_cloz is already in anki or not"""
