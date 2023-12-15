f1131c7dd0b212b8b82e5582942afc52895c4c2e
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-15 19:31:45 +0100

    disable some filters from audio

--- utils/shared_module.py
+++ utils/shared_module.py
@@ -21,15 +21,15 @@ class SharedModule:
 
     # sox effect when loading a sound
     preprocess_sox_effects = [
-            # isolate voice frequency
-            # -2 is for a steeper filtering
-            ["highpass", "-1", "100"],
-            ["lowpass", "-1", "3000"],
-            # removes high frequency and very low ones
-            ["highpass", "-2", "50"],
-            ["lowpass", "-2", "5000"],
-            # # normalize audio
-            ["norm"],
+            # # isolate voice frequency
+            # # -2 is for a steeper filtering
+            # ["highpass", "-1", "100"],
+            # ["lowpass", "-1", "3000"],
+            # # removes high frequency and very low ones
+            # ["highpass", "-2", "50"],
+            # ["lowpass", "-2", "5000"],
+            # # # normalize audio
+            # ["norm"],
             # max silence should be 1s
             ["silence", "-l", "1", "0", "1%", "-1", "1.0", "1%"],
 
