f8d915e5e07b8f1b9863984e8e0c21a17679d80d
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-16 13:04:05 +0100

    disable tqdm if only one file to split

--- utils/audio_splitter.py
+++ utils/audio_splitter.py
@@ -141,7 +141,7 @@ class AudioSplitter:
                     whi(f"File size of {file} is now {fsize}Mb")
 
         # splitting the long audio
-        for iter_file, file in enumerate(tqdm(self.to_split, unit="file", desc="Splitting file", disable=bool(len(self.to_split)-1))):
+        for iter_file, file in enumerate(tqdm(self.to_split, unit="file", desc="Splitting file", disable=not bool(len(self.to_split)-1))):
             whi(f"Splitting file {file}")
             try:
                 if self.stop_source == "replicate":
