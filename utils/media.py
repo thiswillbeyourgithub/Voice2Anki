dd5e66e56797907bb0d8c719d25e55a48ed04d5f
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-22 14:27:29 +0100

    remove cache from soundprocessing

--- utils/media.py
+++ utils/media.py
@@ -150,7 +150,6 @@ def reset_audio():
     return None, None, None, None, None
 
 @trace
-@soundpreprocess_cache.cache
 def sound_preprocessing(audio_mp3_path):
     "removing silence, maybe try to enhance audio, apply filters etc"
     whi(f"Preprocessing {audio_mp3_path}")
