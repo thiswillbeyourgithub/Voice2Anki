2353f927478458b87c0d710e5b1fe269b254a4c3
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-22 19:36:08 +0100

    increase number of slots to 5

--- utils/shared_module.py
+++ utils/shared_module.py
@@ -64,7 +64,7 @@ class SharedModule:
     message_buffer = []
     max_message_buffer = 50
 
-    audio_slot_nb = 3
+    audio_slot_nb = 5
     future_gallery_slot_nb = 60
 
     dirload_queue_columns = [
