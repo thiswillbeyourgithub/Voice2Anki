b8d2baec03fb20d9de354f49b6b5adbd96d7f443
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-29 18:49:53 +0100

    fix

--- utils/anki_utils.py
+++ utils/anki_utils.py
@@ -255,7 +255,7 @@ def get_anki_tags():
 def get_decks():
     try:
         return _call_anki(
-                action="decknames",
+                action="deckNames",
                 )
     except Exception as err:
         return [red(f"Error when getting list of anki deck: {err}'")]
