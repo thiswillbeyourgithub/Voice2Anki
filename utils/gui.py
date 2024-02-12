2a6e6ea3c07ee2c8e64e199081e7f64b8c679c3c
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-02-12 18:52:41 +0100

    fix: only run ocr on the first future gallery

--- utils/gui.py
+++ utils/gui.py
@@ -614,8 +614,8 @@ with gr.Blocks(
                 queue=True,
                 )
 
-        # for the first few galleries only: run ocr in advance
-        if qg_cnt <= 2:
+        # for the first gallery only: run ocr in advance
+        if qg_cnt == 0:
             gal_.change(
                     fn=get_img_source,
                     inputs=[gal_],
