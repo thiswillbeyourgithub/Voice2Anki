8b43e4ff9e784391d0d41673b516616660957f63
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-02-15 13:51:57 +0100

    fix: shift+m to mark

--- utils/gui.py
+++ utils/gui.py
@@ -64,7 +64,7 @@ function shortcuts(e) {
             //document.getElementById("syncankibtn").click();
             document.getElementById("suspendpreviousbtn").click();
         }
-        else if (e.code == ";" && e.shiftKey) {
+        else if (e.key == ';' && e.shiftKey) {
             document.getElementById("markpreviousbtn").click();
         }
         else if (e.key == "s") {
