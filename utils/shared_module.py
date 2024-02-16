eee834634eb00f33f8cbddae7d0cca5961e50822
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-02-16 13:12:27 +0100

    fix: better splitter sox effects I think

--- utils/shared_module.py
+++ utils/shared_module.py
@@ -122,16 +122,16 @@ class SharedModule:
             ["norm"],  # normalize audio
 
             # isolate voice frequency
-            ["highpass", "-1", "100"],
-            ["lowpass", "-1", "3000"],
+            # ["highpass", "-1", "100"],
+            # ["lowpass", "-1", "3000"],
             # -2 is for a steeper filtering: removes high frequency and very low ones
-            # ["highpass", "-2", "50"],
-            # ["lowpass", "-2", "5000"],
+            ["highpass", "-2", "50"],
+            ["lowpass", "-2", "5000"],
 
-            ["norm"],  # normalize audio
+            # ["norm"],  # normalize audio
 
-            # max silence should be 3s
-            ["silence", "-l", "1", "0", "0.1%", "-1", "2.0", "0.1%"],
+            # max silence should be 2s
+            ["silence", "-l", "1", "0", "0.2%", "-1", "2.0", "0.2%"],
 
             # ["norm"],
             ]
