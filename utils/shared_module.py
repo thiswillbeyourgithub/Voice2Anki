f6dd52ea262c9e7fe643e1ce50d9f8ea597958aa
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-06-14 22:36:56 +0200

    better audio splitter

--- utils/shared_module.py
+++ utils/shared_module.py
@@ -125,21 +125,21 @@ class SharedModule:
 
     # sox effects when splitting long audio
     splitter_sox_effects = [
-            ["norm"],  # normalize audio
+            # ["norm"],  # normalize audio
 
             # isolate voice frequency
             # ["highpass", "-1", "100"],
             # ["lowpass", "-1", "3000"],
             # -2 is for a steeper filtering: removes high frequency and very low ones
-            ["highpass", "-2", "50"],
-            ["lowpass", "-2", "5000"],
+            # ["highpass", "-2", "50"],
+            # ["lowpass", "-2", "5000"],
 
-            # ["norm"],  # normalize audio
+            ["norm"],  # normalize audio
 
             # max silence should be 2s
             ["silence", "-l", "1", "0", "1%", "-1", "2.0", "1%"],
 
-            # ["norm"],
+            ["norm"],
             ]
 
     max_message_buffer = 20
