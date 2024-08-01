fee43043c9895904e4bcbaf02928cc936bcfee81
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-08-01 18:48:45 +0200

    fix: price calculation

--- utils/memory.py
+++ utils/memory.py
@@ -435,7 +435,7 @@ def display_price(sld_max_tkn: int, llm_choice: str) -> str:
     if isinstance(price, float):
         return f"${price} per second (actual price computation is probably wrong for now!)"
     price_adj = price["input_cost_per_token"] * 0.9 + price["output_cost_per_token"] * 0.1
-    price_per_request = price_adj * sld_max_tkn / 1000
+    price_per_request = price_adj * sld_max_tkn
     price_per_dol = round(1 / price_per_request, 0)
     message = f"Price if all tokens used: ${price_per_request:.5f}."
     message += f"\nRequests per $1: {price_per_dol:.1f} req"
