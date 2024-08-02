60531c85a92d91083dc38b9e475ff4b5082fe55b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-08-02 19:38:20 +0200

    fix: type

--- utils/main.py
+++ utils/main.py
@@ -802,7 +802,7 @@ def alfred(
 @optional_typecheck
 @trace
 @Critical
-def dirload_splitted(checkbox: bool, *audios: Optional[Union[List, bool]]) -> List[Union[dict, gr.Audio, str, bool]]:
+def dirload_splitted(checkbox: bool, *audios: Optional[Union[List, bool]]) -> List[Union[dict, gr.Audio, str, bool, None]]:
     """
     load the audio file that were splitted previously one by one in the
     available audio slots
@@ -954,7 +954,7 @@ def dirload_splitted(checkbox: bool, *audios: Optional[Union[List, bool]]) -> Li
 @optional_typecheck
 @trace
 @Critical
-def dirload_splitted_last(checkbox: bool) -> Optional[Union[str, gr.Audio, dict]]:
+def dirload_splitted_last(checkbox: bool) -> Union[str, gr.Audio, dict, None]:
     """wrapper for dirload_splitted to only load the last slot. This is faster
     because gradio does not have to send all 5 sounds if I just rolled"""
     audios = [True] * (shared.audio_slot_nb - 1) + [None]
