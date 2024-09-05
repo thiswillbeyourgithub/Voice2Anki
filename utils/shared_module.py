f53d1b78c0905583a29cb6fabbca29175e116bd6
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-05 20:10:44 +0200

    change norm factor

--- utils/shared_module.py
+++ utils/shared_module.py
@@ -117,7 +117,7 @@ class SharedModule:
 
     # sox effects when splitting long audio
     splitter_sox_effects: List[str] = [
-            # ["norm"],  # normalize audio
+            ["norm"],  # normalize audio
 
             # isolate voice frequency
             # ["highpass", "-1", "100"],
@@ -125,8 +125,7 @@ class SharedModule:
             # -2 is for a steeper filtering: removes high frequency and very low ones
             # ["highpass", "-2", "50"],
             # ["lowpass", "-2", "5000"],
-
-            ["norm"],  # normalize audio
+            # ["norm"],  # normalize audio
 
             # max silence should be 3s
             ["silence", "-l", "1", "0", "1%", "-1", "3.0", "1%"],
