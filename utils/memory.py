49b19b3838721ef26d4bf571e4f7b8904d01a3bf
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-14 21:10:42 +0200

    avoid creating multiple cards

--- utils/memory.py
+++ utils/memory.py
@@ -50,6 +50,7 @@ You are my excellent assistant Alfred. Your task today is the to transform audio
 <rules>
 - Often the transcribed text will contain mistakes because it couldn't parse technical words, correct those mistakes.
 - If you have to create several flashcards from one transcript, separate them with a line containing "#####".
+- Don't create several cards on your own, only if I explicitely ask you to.
 - If the question implies giving a choice, order the choice by alphabetical order to make sure I don't memorize by heuristics.
 - Throughout this conversation, you will see plenty of examples so be sure to match the format, structure and formulation of the previous examples when replying. This is critical.
 - As long as I don't tell you that your answer is bad, that means your reply was perfect so keep doing the format you used before.
