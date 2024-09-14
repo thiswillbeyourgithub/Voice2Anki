d7c577f40e20de725fb7dd0eebc521c53b139c0d
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-14 21:00:18 +0200

    replace leading dash before cloze in newline

--- utils/main.py
+++ utils/main.py
@@ -793,6 +793,7 @@ def alfred(
     # for cosmetic purposes in the textbox
     cloz = cloz.replace("<br>", "\n")
     cloz = cloz.replace("<br/>", "\n")
+    cloz = cloz.replace("\n- {{c1::", "\n{{c1::- ")
     cloz = cloz.replace("&nbsp;", " ")
     cloz = cloz.replace("#####", "\n#####\n")  # make sure to separate cleanly the clozes
 
